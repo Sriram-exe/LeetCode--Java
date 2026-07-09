9131cca1ff526f54cc68eff4296ea783cf338ebe
Sriram <sriram999y19@gmail.com>
2026-07-09 22:51:49 +0530

    Add lengthOfLIS method to find longest increasing subsequence
    Implement dynamic programming solution for finding the length of the longest increasing subsequence.

---
+++ DynamicProgramming/LongestIncreasingSubsequence_300.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int maxi = 0;
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        for(int idx = 0;idx<n;idx++){
+            for(int prev = 0;prev<idx;prev++){
+                if(nums[prev]<nums[idx]){
+                    if(dp[prev]+1>dp[idx]){
+                        dp[idx] = dp[prev]+1;
+                    }
+                }
+            }
+            maxi = Math.max(dp[idx],maxi);
+        }
+        return maxi;
+    }
+}
+/*Why is this dynamic Programming solution beacuse dp Storing previous indexes */
+/*dp[idx] represents longest increasing subsequence till idx */
