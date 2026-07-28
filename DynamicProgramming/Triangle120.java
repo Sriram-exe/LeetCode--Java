e329eed7fca772919f91ef7b000bb40a093d4030
Sriram <sriram999y19@gmail.com>
2026-07-29 00:15:12 +0530

    Implement minimum path sum in triangle using DP

--- DynamicProgramming/Triangle120.java
+++ DynamicProgramming/Triangle120.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        for(int[] row:dp){
+            Arrays.fill(row,0);
+        }
+        for(int i=n-1;i>=0;i--){
+            dp[n-1][i] = triangle.get(n-1).get(i);
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                int down = triangle.get(i).get(j) + dp[i+1][j];
+                int diag = triangle.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j] = Math.min(down,diag);
+            }
+        }
+        return dp[0][0];
+    }
+}
