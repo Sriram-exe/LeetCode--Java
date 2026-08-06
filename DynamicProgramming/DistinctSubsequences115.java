584f8b39924196f74da38097fd1954c3ae3da602
Sriram <sriram999y19@gmail.com>
2026-08-07 00:30:52 +0530

    Add dynamic programming solution for distinct subsequences
    Implemented dynamic programming solution for counting distinct subsequences of string t in string s.

--- DynamicProgramming/DistinctSubsequences115.java
+++ DynamicProgramming/DistinctSubsequences115.java
@@ -26,3 +26,33 @@ class Solution {
 //Recursion Code 
 //TC : O(M*N)
 //SC : O(N*M) + O(N)
+
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int[] row : dp){
+            Arrays.fill(row,0);
+        }
+        if(s.charAt(0)==t.charAt(0)) dp[1][1] = 1;
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int pick = 0;
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                int notPick = 0;
+                dp[i][j] += dp[i-1][j];
+            }
+        }
+        return dp[n][m];
+    }
+}
+//Done Bottom Up Approach 
+//TC : O(N*M)
+//SC : O(N*M) eleminated Auxaliry Space Complexity 
